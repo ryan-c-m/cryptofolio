7d448c17af1cb24d97f13519dcc45f1b38ca3a32
RYAN\ryan <ryan.c.morris1@gmail.com>
2018-01-28 22:09:29 +1100

    move css lib

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import CoinIndex from "./containers/coin-index";
 import CoinSummary from "./containers/coin-summary";
 import PageHeader from "./components/page-header";
 import MainView from "./containers/main-view";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../node_modules/react-select/dist/react-select.css";
+import "./css/bootstrap.min.css";
+import "./css/react-select.min.css";
 import "./App.css";
 
 class App extends Component {
