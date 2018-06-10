27c5324a713a3d3c948cbcc2a8deac0fcb09f646
Ryan Morris <ryan.c.morris1@gmail.com>
2018-06-10 15:44:25 +1000

    Configure Firebase hosting

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import thunk from "redux-thunk";
 import App from "./App";
 import persistState from "redux-localstorage";
 import * as reducers from "./store/reducers";
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBF4_YYwFzkPcFz-eLOLHzcaAKVNTC9usE",
+  authDomain: "titanium-gantry-205112.firebaseapp.com",
+  databaseURL: "https://titanium-gantry-205112.firebaseio.com",
+  projectId: "titanium-gantry-205112",
+  storageBucket: "titanium-gantry-205112.appspot.com",
+  messagingSenderId: "490373517578"
+};
+firebase.initializeApp(config);
 
 const store = createStore(
   combineReducers(reducers),
